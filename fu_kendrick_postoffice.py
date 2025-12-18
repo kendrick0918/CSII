86b0469fb5aa35b0f19cdca3b7c1fe2de4e0f173
kendrick0918 <kfu28@gcds.net>
2025-12-18 14:57:50 -0500

    Add files via upload

--- fu_kendrick_postoffice.py
+++ fu_kendrick_postoffice.py
@@ -1,8 +1,16 @@
 import random
 
 '''
+Author: Kendrick Fu
+Sources: Mr. Campbell, Julian Toub
+Bugs: No bugs currently, but had issues with if input wasn't an interger or float, fixed
+Description: Calculates and returns cost of shipping a certain type of package
+Date: 10/23/25
 
-pulls userinput from lenght, width, height for package. 
+
+
+
+pulls userinput from length, width, height for package. 
 
 if-elifs are just specifications for making different categories of shipping
 
@@ -14,32 +22,47 @@ if-elifs are just specifications for making different categories of shipping
 '''
 
 def getsize(l,w,h):
+    """
+    Determines the classifcation of mail based on its dimensions
     
+    l (float) length
+    w (float) width
+    h (float) height
+
+    returns: 
+    str: the category of the mail
+        then returns unmailable if dimensions exceed limits
+    """
     if 3.5 <= l <= 4.25 and 3.5 <= w <= 6 and 0.007 <= h <= 0.016:
-        return "Post_card"
+        return "Post Card"
     elif 4.25 < l <= 6 and 6 < w <= 11.5 and 0.007 <= h <= 0.015:
-        return "Large post card"
+        return "Large Post Card"
     elif 3.5 <= l <= 6.125 and 5 <= w <= 11.5 and 0.016 < h <= 0.25:
-        return "Envelope"
+        return "Envolepe"
     elif 6.125 < l <= 24 and 11 <= w <= 18 and 0.25 < h <= 0.5:
-        return "Large envelope"
+        return "Large Envelope"
     elif l + 2 * (w+h) <= 84:
         return "Package"
     elif l + 2 * (w+h) <= 130:
-        return "Large package"
+        return "Large Package"
     else:
-        return "unmailable"
+        return "Unmailable"
     
 '''
 function for determining zipcode codes
 
-first zipcode has to be an integer always (not a float because no decimals)
+Parameters:
+zipcode (int or str): the zip code 
 
-Then define specifications for different zones to ship to
+returns:
+int: the zone number (1-6). returns 0 if the zip code is invalid
 '''
 def get_zip (zipcode):
    
-    zipcode = int(zipcode)
+    try:
+        zipcode = int(zipcode)
+    except ValueError:
+        return 0
 
     if 0 <= zipcode <= 6999:
         return 1
@@ -55,28 +78,29 @@ def get_zip (zipcode):
         return 6
     else:
         return 0  # Invalid zip
-'''
-this function calculates cost
-
-takes the mailtype (if its a certain type decides how much it costs to ship)
-then takes the distance from zipcode (first zone and last zone) (defined below in main)
-Mathmathical calculations for how muc it costs
-
-'''
     
 def calculatecost(mailtype, distance):
-    if mailtype == "Post card":
-        return 0.40 + (0.05 * distance)
-    elif mailtype == "Large post card":
-        return 0.55 + (0.07 * distance)
+
+    """
+    Calculates shipping cost based on mail type and zone distance
+    
+    Parameters:
+    mailtype (string) category of the mail
+    distance (int) The positive/absolute difference between the starting and ending zones.
+    """
+
+    if mailtype == "Post Card":
+        return 0.20 + (0.03 * distance)
+    elif mailtype == "Large Post Card":
+        return 0.37 + (0.03 * distance)
     elif mailtype == "Envelope":
-        return 0.70 + (0.10 * distance)
-    elif mailtype == "Large envelope":
-        return 1.00 + (0.15 * distance)
+        return 0.37 + (0.04 * distance)
+    elif mailtype == "Large Envelope":
+        return 0.60 + (0.05 * distance)
     elif mailtype == "Package":
-        return 3.00 + (0.50 * distance)
+        return 2.95 + (0.25 * distance)
     elif mailtype == "Large package":
-        return 5.00 + (0.75 * distance)
+        return 3.95 + (0.35 * distance)
     else:
         return 0.00
 
@@ -92,33 +116,43 @@ Then pull the calculate cost and print
 '''
 
 def main():
+  
+    #trying to loop the program 5 times 
+    for i in range(5): 
+        print(f"Run{i+1} of 5")
+        data = input("Enter data (Length, Width, Height, ZipTo, ZipFrom)") 
+        data = data.split (",")
+        
+        if len(data) != 5:
+            print("invalid input, input 5 values")
+            return
+        try:
+            l = float(data[0])
+            w = float(data[1])
+            h = float(data[2])
+            zipto = int(data[3])
+            zipfrom = int(data[4])
+        except ValueError:
+            print("Invalid Input, Input must be a number")
+            return
+        zone_to = get_zip(zipto)
+        zone_from = get_zip(zipfrom)
+        distance = abs(zone_to - zone_from)
+        
 
-    data = input("enter data")
-    data = data.split (",")
+        mailtype = getsize(l,w,h)
+        distance = abs(get_zip(zipto)- get_zip(zipfrom))
+        cost = calculatecost(mailtype, distance)
+       
 
-    if len(data) != 5:
-        print("invalid input, input 5 values")
-        return
-    try:
-        l = float(data[0])
-        w = float(data[1])
-        h = float(data[2])
-        zipto = int(data[3])
-        zipfrom = int(data[4])
-    except ValueError:
-        print("Invalid Input, Input must be a number")
-        return
-    zone_to = get_zip(zipto)
-    zone_from = get_zip(zipfrom)
-    distance = abs(zone_to - zone_from)
-    
+        
+        cost = str(cost).lstrip('0')
 
-    mailtype = getsize(l,w,h)
-    distance = abs(get_zip(zipto)- get_zip(zipfrom))
-    cost = calculatecost(mailtype, distance)
-    print("Mail Type:", mailtype)
-    print("Zone Distance:", distance)
-    print("Cost of Delivery: $", cost)
+        if len(cost) == 2:
+            cost = cost + "0"
 
+
+        print(cost)
+     
 if __name__ == "__main__":
     main()
